dc6bb01ded204e58930a85328bc6e841c2b69375
UpsilonSigmaEta <sanghyeon.yoon37@gmail.com>
2025-06-23 17:56:48 +0900

    MainControlPanelView에 전송/수신 메시지 로그 워터마크 추가

--- SourceCode/SCPIMCMain/SCPIMCMain/Model/Implementation/DeviceModel.cs
+++ SourceCode/SCPIMCMain/SCPIMCMain/Model/Implementation/DeviceModel.cs
@@ -1,3 +1,4 @@
+using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using SCPIMCMain.Common.Enum;
@@ -7,20 +8,152 @@ namespace SCPIMCMain.Model.Implementation
 {
     public class DeviceModel : IDeviceModel, ISaveable, ILoadable
     {
-        public string DeviceName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string DeviceType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string IPAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int Port { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public EDeviceConnectionStatus ConnectionStatus { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private string _deviceName;
+        private string _deviceType;
+        private string _ipAddress;
+        private int _port;
+        private EDeviceConnectionStatus _connectionStatus;
+        private TcpClient _tcpClient;
+
+        public TcpClient TcpClient
+        {
+            get
+            {
+                return _tcpClient;
+            }
+            protected set
+            {
+                if (_tcpClient != null && value == null)
+                {
+                    throw new Exception("Client must be non-null after initialized.");
+                }
+                else
+                {
+                    _tcpClient = value;
+                }
+            }
+        }
+
+        public string DeviceName
+        {
+            get => _deviceName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Device name cannot be null or empty.");
+                }
+                _deviceName = value;
+            }
+        }
+        public string DeviceType
+        {
+            get => _deviceType;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Device Type cannot be null or empty.");
+                }
+                _deviceType = value;
+            }
+        }
+        public string IPAddress
+        {
+            get => _ipAddress;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("IP Address cannot be null or empty.");
+                }
+                _ipAddress = value;
+            }
+        }
+        public int Port
+        {
+            get => _port;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Port number cannot be below zero.");
+                }
+                _port = value;
+            }
+        }
+        public EDeviceConnectionStatus ConnectionStatus
+        {
+            get
+            {
+                return _connectionStatus;
+            }
+            set
+            {
+                _connectionStatus = value;
+            }
+        }
 
         public EDeviceConnectionStatus Connect(string ipAddress, int port)
         {
-            throw new NotImplementedException();
+            // Connect할 때 한 번만 IPAddress와 Port 값을 설정
+            // 한 번 설정되고 나서는 변경할 수 없음.
+            if (IPAddress == null)
+            {
+                IPAddress = ipAddress;
+            }
+            if (Port == 0)
+            {
+                Port = port;
+            }
+
+            return Connect();
         }
 
         public EDeviceConnectionStatus Connect()
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (_tcpClient == null)
+                {
+                    _tcpClient = new TcpClient(IPAddress, Port);
+
+                    _connectionStatus = EDeviceConnectionStatus.Disconnected;
+
+                    if (_tcpClient != null)
+                    {
+                        _connectionStatus = EDeviceConnectionStatus.Connecting;
+
+                        _tcpClient.Connect(IPAddress, Port);
+
+                        if (_tcpClient.Connected)
+                        {
+                            _connectionStatus = EDeviceConnectionStatus.Connected;
+
+                            return _connectionStatus;
+                        }
+                        else
+                        {
+                            _connectionStatus = EDeviceConnectionStatus.Disconnected;
+
+                            return _connectionStatus;
+                        }
+                    }
+                }
+
+                return _connectionStatus;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Can not connect to host device. Reason: {ex.Message}");
+
+                if (_connectionStatus == EDeviceConnectionStatus.Connecting)
+                {
+                    Disconnect();
+                }
+
+                return _connectionStatus;
+            }
         }
 
         public Task<EDeviceConnectionStatus> ConnectAsync(string ipAddress, int port, CancellationToken cts)
